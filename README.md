6d4cb36e98c27a7086aa48f4c555ca1ffd02565b
Alban King <alban.king@zervant.com>
2023-03-03 12:05:15 +0200

    add subcommand specific defaults

--- README.md
+++ README.md
@@ -16,17 +16,41 @@ Running awsdefault in `my_project` will collect the defaults from `.awsdefault (
 				└── .awsdefault (D)
 ~~~
 
-`.awsdefault` must have a section called `default`. From there, keys are interpreted as keyword arguments, adding `--` to the start of them and appending them to the final `aws` command.
+`.awsdefault` should have sections named with aws sub commands, with the exception of `[default]` which will be added to all commands.
+ From there, keys are interpreted as keyword arguments, adding `--` to the start of them and appending them to the final `aws` command.
 
 Example `.awsdefault` file:
 ~~~
-cat .awsdefault
+$ cat .awsdefault
 
 [default]
 profile=my-fave-profile
 region=eu-central-1
 cluser=cluster-name
+
+[logs get-log-events]
+log-group-name=my-log-group
+log-stream-name=random-log-stream-123
 ~~~
+
+~~~
+$ AWSDEFAULT_DEBUG=1 awsdefault logs get-log-events
+using default args from files: /media/alban/Shared/Shared_Documents/Zervant/Projects/awsdefault/.awsdefault
+adding '--profile example_profile'
+adding '--log-stream-name random-log-stream-123'
+adding '--log-group-name my-log-group'
+executing: aws logs get-log-events --profile example_profile --log-stream-name random-log-stream-123 --log-group-name my-log-group
+
+...
+
+$ AWSDEFAULT_DEBUG=1 awsdefault help               
+using default args from files: /media/alban/Shared/Shared_Documents/Zervant/Projects/awsdefault/.awsdefault
+adding '--profile example_profile'
+executing: aws help --profile example_profile
+
+...
+~~~
+
 
 If an argument is literally specified when calling `awsdefault` it will not be replaced by any default.
 If a default argument is specified in two `.awsdefault` files, the value on the deeper path will be used.
@@ -37,8 +61,8 @@ This allows for default arguments to be set at any project level.
 
 ## installation + setup
 ~~~ 
-pip install awsdefault
-alias aws=awsdefault
+$ pip install awsdefault
+$ alias aws=awsdefault
 ~~~
 
 
--- awsdefault.py
+++ awsdefault.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import configparser
 from pathlib import Path, PurePath
+from typing import Generator
 
 
 DEBUG = os.getenv("AWSDEFAULT_DEBUG")
@@ -20,21 +21,25 @@ else:
     from os import execvp
 
 
+def read_config(path: Path | str) -> Generator[tuple[tuple, dict], None, None]:
+	awsdefault = configparser.ConfigParser()
+	awsdefault.read(path)
+	for section in awsdefault.sections():
+		yield tuple(section.strip().split()), dict(awsdefault.items(section)) 
+
+
 def assemble_default_config(path) -> tuple[dict, list[str]]:
-	defaults = {}
+	defaults: dict[tuple, dict[str, str]] = {}
 	config_files = []
 	_path = PurePath()
 
 	for part in path.parts:
 		_path = _path.joinpath(part)
-
 		if ".awsdefault" in (p.name for p in Path(_path).iterdir()):
 			if Path(_path.joinpath(".awsdefault")).is_file():
-				awsdefault = configparser.ConfigParser()
-				awsdefault.read(Path(_path.joinpath(".awsdefault")))
-				defaults = {**defaults, **dict(awsdefault.items("default"))}
-
 				config_files.append(str(Path(_path.joinpath(".awsdefault"))))
+				for section, _defaults in read_config(Path(_path.joinpath(".awsdefault"))):
+					defaults[section] = {**defaults.get(section, {}), **_defaults}
 
 	return defaults, config_files
 
@@ -44,18 +49,24 @@ def main() -> int:
 	cmd[0] = "aws"
 
 	if not DISABLE:
-		# default_args, config_file  = get_most_local_config(Path(os.getcwd()))
 		default_args, config_files = assemble_default_config(Path(os.getcwd()))
 
 		if DEBUG:
 			print(f"using default args from files: {', '.join(config_files)}", file=sys.stderr)
 
-		for arg, val in default_args.items():
-			arg = "--"+arg
-			if arg not in cmd:
-				cmd.extend([arg, val])
-				if DEBUG:
-					print(f"adding '{arg} {val}'", file=sys.stderr)
+		subcommand = tuple(arg for arg in cmd[1:3] if not arg.startswith("-"))
+
+		for section, arguments in default_args.items():
+			if section == ("default",) or section == subcommand:
+				for key, val in arguments.items():
+					key = f"--{key}"
+					if key not in cmd:
+						cmd.extend([key, val])
+						if DEBUG:
+							print(f"adding '{key} {val}'", file=sys.stderr)
+
+	if DEBUG:
+		print(f"executing: {' '.join(cmd)}", file=sys.stderr)
 
 	return execvp(cmd[0], cmd)
 
